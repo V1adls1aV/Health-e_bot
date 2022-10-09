0312bacf21177d7e58a282e23b7520b49b3c7c21
alexdremov <dremov.me@gmail.com>
2022-10-10 00:44:12 +0300

    Added SQL DB scheme

---
+++ db_scheme.sql
@@ -0,0 +1,53 @@
+/*
+ Date: 10/10/2022 00:42:54
+*/
+
+PRAGMA foreign_keys = false;
+
+-- ----------------------------
+-- Table structure for connections
+-- ----------------------------
+DROP TABLE IF EXISTS "connections";
+CREATE TABLE connections (user_id INT REFERENCES users (user_id) NOT NULL, exception_id INT REFERENCES exceptions (exception_id) NOT NULL, connection_creating_date DATETIME);
+
+-- ----------------------------
+-- Table structure for e_additions
+-- ----------------------------
+DROP TABLE IF EXISTS "e_additions";
+CREATE TABLE "e_additions" (
+  "e_number" STRING NOT NULL,
+  "e_name" STRING,
+  "harm" STRING,
+  "property" STRING,
+  "usage" STRING,
+  "influence" STRING,
+  PRIMARY KEY ("e_number")
+);
+
+-- ----------------------------
+-- Table structure for exceptions
+-- ----------------------------
+DROP TABLE IF EXISTS "exceptions";
+CREATE TABLE exceptions (exception_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE, exception_name STRING);
+
+-- ----------------------------
+-- Table structure for sqlite_sequence
+-- ----------------------------
+DROP TABLE IF EXISTS "sqlite_sequence";
+CREATE TABLE sqlite_sequence(name,seq);
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS "users";
+CREATE TABLE users (user_id INTEGER NOT NULL UNIQUE PRIMARY KEY AUTOINCREMENT, user_name STRING, user_creating_date DATETIME);
+
+-- ----------------------------
+-- Auto increment value for exceptions
+-- ----------------------------
+
+-- ----------------------------
+-- Auto increment value for users
+-- ----------------------------
+
+PRAGMA foreign_keys = true;
