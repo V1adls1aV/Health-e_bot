42d41ccd321d069c1112dd13830703e017bfce5f
Владислав <V1adls1aV@yandex.ru>
2022-10-30 20:32:52 +0300

    Creating Classes

--- db_editors.py
+++ db_editors.py
@@ -1,32 +1,100 @@
+from ast import Delete
 from sql_objects import User, Exception, Connection
-from sqlalchemy import *
-from sqlalchemy.orm import *
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import Session
+from datetime import datetime
 
 
-class UserEditor:
-    def __init__(self) -> None:
-        pass
+class UserEditor:  # Продумать все взаимодействия с классом, над тем, что должны возвращать методы.
+    engine = create_engine("sqlite:///data/Users_data.db", echo=True, future=True)
 
-    def create_user(self):
-        pass
+    def create_user(self, user_name=str) -> int:
+        with Session(self.engine) as session:
+            user = User(
+                user_name=user_name,
+                user_creating_date=datetime.now()
+            )
+            session.add(user)
+            session.commit()
+        return user.user_id
 
-    def remove_user(self):
-        pass
+    def remove_user(self, user_id=int):
+        with Session(self.engine) as session:
+            user = session.scalar(
+                select(User).where(User.user_id == user_id)
+            )
 
-    def create_connection(self):
-        pass
+            for con in user.connection:
+                session.delete(con)
 
-    def get_all_exceptions(self):
-        pass
+            session.delete(user)
+            session.commit()
+
+    def get_user_id(self, user_name=str) -> int:
+        with Session(self.engine) as session:
+            return session.scalar(
+                select(User.user_id).where(User.user_name == user_name)
+            )
+
+    def get_all_exceptions(self, user_id=int) -> list:
+        with Session(self.engine) as session:
+            return session.scalars(
+                select(Exception.exception_name)
+                .join(Exception.connection)
+                .where(Connection.user_id == user_id)
+            ).all()
+            
+    def create_connection(self, user_id=int, exception_id=int):
+        with Session(self.engine) as session:
+            session.add(Connection(
+                user_id=user_id,
+                exception_id=exception_id,
+                connection_creating_date=datetime.now()
+            ))
+            session.commit()
+
+    def remove_connection(self, user_id=int, exception_id=int):
+        with Session(self.engine) as session:
+            session.delete(session.scalar(
+                select(Connection).where(Connection.user_id == user_id).where(
+                    Connection.exception_id == exception_id)
+            ))
+            session.commit()
 
 
 class ExceptionEditor:
+    engine = create_engine("sqlite:///data/Users_data.db", echo=True, future=True)
+
+    def create_exception(self, exception_name=str) -> int:
+        with Session(self.engine) as session:
+            exception = Exception(
+                exception_name=exception_name
+            )
+            session.add(exception)
+            session.commit()
+        return exception.exception_id
+
+    def remove_exception(self, exception_id=int):
+        with Session(self.engine) as session:
+            exception = session.scalar(
+                select(Exception).where(Exception.exception_id == exception_id)
+            )
+
+            if exception:
+                raise UserConnectionExist()
+
+            session.delete(exception)
+            session.commit()
+
+    def get_exception_id(self, exception_name=str) -> int:
+        with Session(self.engine) as session:
+            return session.scalar(
+                select(Exception.exception_id).where(Exception.exception_name == exception_name)
+            )
+
+
+class UserConnectionExist(BaseException):
     def __init__(self) -> None:
-        pass
-
-    def create_exception(self):
-        pass
-
-    def remove_exception(self):
-        pass
-
+        super().__init__(
+            'You have to remove user connections to this exception before deleting it.'
+        )
--- main.py
+++ main.py
@@ -1,27 +1,8 @@
-from sqlalchemy import create_engine, select
-from sqlalchemy.orm import Session
-from datetime import datetime
-from sql_objects import User, Exception, Connection
+from db_editors import UserEditor, ExceptionEditor
 
 
-engine = create_engine("sqlite:///data/Users_data.db", echo=True, future=True)
+ud = UserEditor()
+ed = ExceptionEditor()
 
-with Session(engine) as session:
-    """
-    u = User(
-        user_name='V4',
-        user_creating_date=datetime.now()
-    )
-    
-    e = Exception(
-        exception_name='nuts'
-    )"""
+ed.remove_exception(1)
 
-    c = Connection(
-        user_id=session.scalar(select(User.user_id).where(User.user_name == 'V2')),
-        exception_id=session.scalar(select(Exception.exception_id).where(Exception.exception_name == 'milk')),
-        connection_creating_date=datetime.now()
-    )
-
-    session.add(c)
-    session.commit()
