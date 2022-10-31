5a5556f8604470d9ec71121df3966b0955725c03
Владислав <V1adls1aV@yandex.ru>
2022-10-31 20:38:36 +0300

    telegram_bot_structure

--- main.py
+++ main.py
@@ -1,20 +1,73 @@
-import sqlite3
-import csv
-
-with open('new_additions.csv', encoding='utf-8') as file:
-    items = list(csv.reader(file, delimiter=',', quotechar='"'))
-items = list(filter(lambda x: x, items))
+import telebot as tb
+from telebot import types
+from data.config import TOKEN, DESCRIPTION, ADMINNAME
 
 
-db = sqlite3.connect('Users_data.db')
-curs = db.cursor()
+bot = tb.TeleBot(TOKEN, parse_mode='HTML')
 
-for el in items:
-    curs.execute(
-    f"""
-    INSERT INTO e_additions (e_number, e_name, harm, property, usage, influence)
-    VALUES {tuple(el)}
-    """)
 
-db.commit()
-db.close()
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add(
+        types.KeyboardButton('Проверить состав'),
+        types.KeyboardButton('Чёрный список')
+    )
+
+    # if message.from_user.username not in db: ...  (Add user)
+    
+    bot.send_message(message.chat.id, 
+    DESCRIPTION.format(message.from_user.username), reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def buttons_handler(message):
+    if message.text == 'Проверить состав':
+        bot.send_message(message.chat.id,
+        'Пришлите фоторграфию или текст состава.')
+
+    elif message.text == 'Чёрный список':
+        markup = types.InlineKeyboardMarkup()
+
+        markup.add(
+            types.InlineKeyboardButton('Получить список', callback_data='get'),
+            types.InlineKeyboardButton('Добавить элемент', callback_data='add'),
+            types.InlineKeyboardButton('Удалить элемент', callback_data='del')
+        )
+
+        bot.send_message(message.chat.id, 'Выберете действие:', reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text', 'image'])
+def structure_analyser(message):
+    if message.content_type == 'text':  # Call text analyser (with all user exceptions)
+        pass
+    elif message.content_type == 'image':  # AI
+        pass
+
+
+@bot.callback_query_handler(func=lambda c: True)
+def database_handler(call):
+    if call.message:
+        if call.data == 'get':  # Get all exceptions
+            pass
+        elif call.data == 'add':  # Add a connection/excepiton
+            pass
+        elif call.data == 'del':  # Delete connection/exception
+            pass
+
+
+@bot.message_handler(commands=['distribute'])
+def distribute_news(message):
+    pass
+    """
+    if message.from_user == ADMINNAME:
+        for user in users:
+            try:
+                bot.send_message(user, message.text)
+            except:
+                remove_user()
+    """
+
+
+bot.infinity_polling()  # Running
