56f84b3aa802ba7ef3a807421b6a043eb8570702
V1adls1aV <V1adls1aV@yandex.ru>
2023-01-03 10:27:10 +0300

    GitHub Actions

--- tests/e2e_tests/setup.py
+++ tests/e2e_tests/setup.py
@@ -1,8 +1,10 @@
 import pytest_asyncio
 from telethon import TelegramClient
-from tests.e2e_tests.data.config import API_HASH, API_ID
+from os import environ
 
 
 @pytest_asyncio.fixture
 async def client() -> TelegramClient:
-    return TelegramClient('testing', API_ID, API_HASH)
+    return TelegramClient('testing', 
+    environ.get('API_ID'), 
+    environ.get('API_HASH'))
