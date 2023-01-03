3032ddf50db95adb8156626057da2bc54d745312
V1adls1aV <V1adls1aV@yandex.ru>
2023-01-03 23:26:05 +0300

    Fixing .gitignore and improving tests

--- tests/e2e_tests/setup.py
+++
@@ -1,8 +0,0 @@
-import pytest_asyncio
-from telethon import TelegramClient
-from tests.e2e_tests.data.config import API_ID, API_HASH
-
-
-@pytest_asyncio.fixture
-async def client() -> TelegramClient:
-    return TelegramClient('testing', API_ID, API_HASH)
