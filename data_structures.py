311ea7cc33e68f1b89b8e77cba23e5f235105484
V1adls1aV <V1adls1aV@yandex.ru>
2023-01-03 10:39:26 +0300

    GitHub Actions

--- data_structures.py
+++ data_structures.py
@@ -1,3 +1,5 @@
+from tests.unit_tests.setup import UserMock
+from objects.user import User
 from objects.ecode import ECode
 from data.config import TESS_CONFIG
 from datetime import datetime
@@ -99,7 +101,7 @@ class Composition(AdditiveList):
                     eco.add(self.user_ecodes[n])
         return list(ad), list(eco)
 
-    def set_user(self, user):
+    def set_user(self, user: UserMock):
         self.chat_id = user.chat_id
         self.user_additives = user.get_additives_names()
         self.additives, self.ecodes = self._find_additives()
